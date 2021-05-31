9390ca7ffcc1211bd86fd9ad58a752dbdc3ae60b
satotory <ruskosha@gmail.com>
2021-05-31 23:55:05 +0300

    little changes home

---
+++ _site/assets/mainstr.js
@@ -0,0 +1,9 @@
+function openbox(open){
+    display = document.getElementById('titlebox').style.display;
+    if(display == &quot;none&quot;) {
+    document.getElementById('titlebox').style.display =&quot;block&quot;;
+    }
+    else{
+    document.getElementById('titlebox').style.display =&quot;none&quot;;
+    }
+    }
