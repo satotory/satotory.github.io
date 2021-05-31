b6ab95ad3c0cf9c145e922c25067ef362b500f31
satotory <ruskosha@gmail.com>
2021-06-01 01:18:11 +0300

    list changed

--- _site/assets/mainstr.js
+++
@@ -1,9 +0,0 @@
-function openbox(open){
-    display = document.getElementById('titlebox').style.display;
-    if(display == &quot;none&quot;) {
-    document.getElementById('titlebox').style.display =&quot;block&quot;;
-    }
-    else{
-    document.getElementById('titlebox').style.display =&quot;none&quot;;
-    }
-    }
